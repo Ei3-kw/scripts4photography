7e0276b91577bb6779f99f700a5ac42bc59b964b
Ei3 <e013.kw@gmail.com>
2024-08-06 15:17:54 +1000

    split and glue derangedly

--- panorama.py
+++ panorama.py
@@ -2,8 +2,7 @@ from PIL import Image
 import math
 from startrace import select_images
 
-def split_image(image_path, n):
-    image = Image.open(image_path[0])
+def split_image(image, n, save=True):
     width, height = image.size
 
     sect_width = width // n
@@ -16,7 +15,8 @@ def split_image(image_path, n):
         bottom = height
 
         result = image.crop((left, top, right, bottom))
-        result.save(f"{i}.png")
+        if save:
+            result.save(f"{i}.png")
         results.append(result)
 
     return results
@@ -36,18 +36,62 @@ def glue_images_vertically(images):
 
     result.save("result.png")
 
+def glue_images_horizontally(images):
+    widths, heights = zip(*(img.size for img in images))
+
+    max_height = max(heights)
+    total_width = sum(widths)
+
+    result = Image.new('RGB', (total_width, max_height))
+    x_offset = 0
+    for img in images:
+        width, height = img.size
+        result.paste(img, (x_offset, 0))
+        x_offset += width
+
+    return result
+
+def generate_derangements(n):
+    def backtrack(index, current, used):
+        if index == n:
+            derangements.append(current[:])
+            return
+
+        for i in range(n):
+            if not used[i] and i != index:
+                used[i] = True
+                current[index] = i
+                backtrack(index + 1, current, used)
+                used[i] = False
+
+    derangements = []
+    backtrack(0, [0] * n, [False] * n)
+    return derangements
+
+def split_N_glue(images):
+    turtlecat = []
+    n = len(images)
+    for img in images:
+        turtlecat.append(split_image(img, n, False))
+
+    for d in generate_derangements(n):
+        imgs = [turtlecat[d[i]][i] for i in range(n)]
+        glue_images_horizontally(imgs).save(f"{d}.png")
+
 
 if __name__ == '__main__':
     try:
-        match input('1: split panaroma \n2: merge vertically'):
+        match input('1: split panaroma \n2: merge vertically \n3: split n photos into n parts and glue derangely'):
             case '1':
-                results = split_image(select_images(), int(input("number of sections:")))
+                results = split_image(Image.open(select_images()[0]), int(input("number of sections:")))
 
                 if input("do U wanna merge them vertically? Y/N") not in "Nn":
                     glue_images_vertically(results)
             case '2':
                 images = [Image.open(i) for i in select_images()]
                 glue_images_vertically(images)
+            case '3':
+                split_N_glue([Image.open(i) for i in select_images()])
 
     except ValueError as e:
         print("Error:", e)
